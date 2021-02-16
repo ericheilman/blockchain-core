0fa8176d61525b737ce8245f802d27bce6e980a0
Eric Heilman <>
2021-02-16 02:56:09 -0600

    Testing get_split function for blockchain_ledger_gateway_v2.erl

--- src/ledger/v1/blockchain_ledger_gateway_v2.erl
+++ src/ledger/v1/blockchain_ledger_gateway_v2.erl
@@ -185,7 +185,7 @@ get_splits(Gateway) ->
 %% sort reward map list
 -spec set_split(Gateway :: gateway(), OwnerAddress :: libp2p_crypto:pubkey_bin(), RewardSplit :: non_neg_integer()) -> rewards_map().
 set_split(Gateway, OwnerAddress, RewardSplit) ->
-  Gateway#gateway_v2{rewards_map = lists:keyreplace(OwnerAddress, 1,Gateway#gateway_v2.rewards_map,{OwnerAddress,RewardSplit})}.
+  Gateway#gateway_v2{rewards_map = lists:keyreplace(OwnerAddress,Gateway#gateway_v2.rewards_map,{OwnerAddress,RewardSplit})}.
 
 
 -spec num_splits(Gateway :: gateway()) -> [non_neg_integer()].
