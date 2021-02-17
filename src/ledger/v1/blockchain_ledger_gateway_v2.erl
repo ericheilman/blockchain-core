69f02b0b98266e6bac7953d1ef3dc8e741ad45e5
Eric Heilman <>
2021-02-17 15:51:25 -0600

    no message

--- src/ledger/v1/blockchain_ledger_gateway_v2.erl
+++ src/ledger/v1/blockchain_ledger_gateway_v2.erl
@@ -617,7 +617,7 @@ deserialize(<<2, Bin/binary>>) ->
   RewardsMap = rewards_map(Gw2),
   RewardsFinal = lists:foldl(
         fun(Reward,RewardsList) ->
-                case type(Reward) == blockchain_ledger_gateway_rewards_map of
+                case is_list(Reward) of
                     false ->
                         RewardsList;
                     true ->
