60f9101762e32adbf85837885b553796e351b156
Eric Heilman <>
2021-02-18 05:38:52 -0600

    no message

--- src/transactions/v1/blockchain_txn_split_rewards_v1.erl
+++ src/transactions/v1/blockchain_txn_split_rewards_v1.erl
@@ -363,8 +363,6 @@ buyer_nonce_correct(#blockchain_txn_split_rewards_v1_pb{buyer_nonce=Nonce,
             Nonce =:= blockchain_ledger_entry_v1:nonce(Entry) + 1
     end.
 
-
--spec get_config_or_default(txn_split_rewards(), blockchain_ledger_v1:ledger()) ->
 get_config_or_default(?transfer_hotspot_stale_poc_blocks=Config, Ledger) ->
     case blockchain_ledger_v1:config(Config, Ledger) of
         {error, not_found} -> ?STALE_POC_DEFAULT;
